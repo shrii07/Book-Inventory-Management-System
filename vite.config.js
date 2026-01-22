cf5c0ed5d2b54934657e9527b5055c0241cc009a
Shrikant Tangadkar <142814659+shrii07@users.noreply.github.com>
2026-01-22 22:21:41 +0530

    Update vite.config.jsdone

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Book-Inventory-Management-System/',
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: '/',   // ✅ REQUIRED for Vercel
 })
